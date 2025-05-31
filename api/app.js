9e3d52ecb2a2889175ef94922e9cb85980ba241a
JGuilherme-aa1603 <joaoguilherme.aa1603@gmail.com>
2025-05-31 11:44:52 -0300

    chore: add CORS middleware to app.js for cross-origin requests

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 //Adiciona após a conexão com o MongoDB
 const express = require('express');
 const postRoutes = require('./routes/postRoutes.js');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/posts', postRoutes);
 
